78d7f9fc32e0d079f69e41b9a6799dd429197ce8
Victor Kotseruba <barbuzaster@gmail.com>
2017-06-14 23:43:46 -0600

    gardening

--- examples/redux/components/ActivityIndicator.tsx
+++ examples/redux/components/ActivityIndicator.tsx
@@ -1,21 +1,27 @@
 import * as React from "react";
 import { connect } from "react-redux";
+import { style } from "typestyle";
+
 import { IReduxState } from "../reducer";
 import { isAsyncDataLoading } from "../selectors";
+
+const indicator = style({
+  position: "fixed",
+  right: 10,
+  top: 10,
+});
 
 class ActivityIndicatorComponent extends React.PureComponent<{ loading: boolean }, object> {
 
   public render() {
-    let display: string = "none";
     if (this.props.loading) {
-      display = "block";
+      return (
+        <div className={indicator}>
+          loading
+        </div>
+      );
     }
-
-    return (
-      <div style={{ position: "fixed", top: 10, right: 10, display }}>
-        loading
-      </div>
-    );
+    return null;
   }
 
 }
